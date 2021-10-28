5515e21118e0e6f4229a209807d49dabcd9eb5e9
rpashev <rossen1991@gmail.com>
2021-10-28 15:13:06 +0300

    tried to fix vulnerabilities, turns out you can't, reset everything + sass finaly works

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import styles from './MovieCard.module.css'
+import styles from './MovieCard.module.scss'
 
 const MovieCard = (props) => {
   return (
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import userContext from "./Context/user-context";
 import App from "./App";
 
-const user = true;
+const user = false;
 ReactDOM.render(
   <userContext.Provider value={{ user }}>
     <App />
