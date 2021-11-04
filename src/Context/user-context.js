741926747ee1093f32f4b863521dc4c8b0234909
rpashev <rossen1991@gmail.com>
2021-11-04 15:28:23 +0200

    added context login/logout logic

--- src/Context/user-context.js
+++ src/Context/user-context.js
@@ -1,7 +1,67 @@
-import React from 'react'
+import React, { useState } from "react";
 
+const AuthContext = React.createContext({
+  token: "",
+  isLoggedIn: false,
+  username: "",
+  userId: "",
+  email: "",
+  login: (token, username, userId, email) => {},
+  logout: () => {},
+});
 
-const userContext = React.createContext({
-   user: null,
-})
-export default userContext
+export const AuthContextProvider = (props) => {
+  const initialToken = localStorage.getItem("token");
+  const initialUsername = localStorage.getItemn("username");
+  const initialUserId = localStorage.getItem("userId");
+  const initialEmail = localStorage.getItem("email");
+
+  const [token, setToken] = useState(initialToken);
+  const [username, setUsername] = useState(initialUsername);
+  const [userId, setUserId] = useState(initialUserId);
+  const [email, setEmail] = useState(initialEmail);
+
+  const userIsLoggedIn = !!token;
+
+  const loginHandler = (token, username, userId, email) => {
+    setToken(token);
+    setUsername(username);
+    setUserId(userId);
+    setEmail(email);
+
+    localStorage.setItem("token", token);
+    localStorage.setItem("username", username);
+    localStorage.setItem("userId", userId);
+    localStorage.setItem("email", email);
+  };
+
+  const logoutHandler = () => {
+    setToken(null);
+    setUsername("");
+    setUserId("");
+    setEmail("");
+
+    localStorage.removeItem("token");
+    localStorage.removeItem("username");
+    localStorage.removeItem("userId");
+    localStorage.removeItem("email");
+  };
+
+  const contextValue = {
+    token,
+    isLoggedIn: userIsLoggedIn,
+    email,
+    username,
+    userId,
+    login: loginHandler,
+    logout: logoutHandler,
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
