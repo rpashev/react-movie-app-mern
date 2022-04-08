0dcb296f2c884e29010aa47a21c241b7027c0381
rpashev <rossen1991@gmail.com>
2022-04-08 17:15:47 +0300

    redesign of features section

--- src/components/landing-page/Features/Features.js
+++ src/components/landing-page/Features/Features.js
@@ -7,27 +7,33 @@ const Features = () => {
       <ul className={styles.features}>
         <li className={styles.feature}>
           <ion-icon name="search-outline"></ion-icon>
-          <p>Use the OMDB database to find any movie or tv series</p>
+          <h3>Large movie database</h3>
+          <p>Use the OMDB database to find any movie or tv series that you can think of.</p>
         </li>
         <li className={styles.feature}>
           <ion-icon name="build-outline"></ion-icon>
-          <p>Build your own watchlist and keep track of movies you've watched</p>
+          <h3>Build a watchlist</h3>
+          <p>Build your own watchlist and keep track of movies you have watched.</p>
         </li>
         <li className={styles.feature}>
           <ion-icon name="eye-outline"></ion-icon>
-          <p>Choose a movie and view its most important details</p>
+          <h3>Movie details</h3>
+          <p>Choose a movie and view its most important details including a plot summary.</p>
         </li>
         <li className={styles.feature}>
           <ion-icon name="people-outline"></ion-icon>
-          <p>See what other users are watching in the "Explore" section</p>
+          <h3>See what's trending</h3>
+          <p>Check out what our other users are watching in the "Explore" section.</p>
         </li>
         <li className={styles.feature}>
           <ion-icon name="person-circle-outline"></ion-icon>
-          <p>Customize your profile by uploading your own avatar</p>
+          <h3>Customize your profile</h3>
+          <p>Personalize your account with us by uploading your very own avatar.</p>
         </li>
         <li className={styles.feature}>
           <ion-icon name="phone-portrait-outline"></ion-icon>
-          <p>Use the app from any device: desktop, tablet or mobile</p>
+          <h3>Full accessibility</h3>
+          <p>Use the application from any device, be it desktop, tablet or mobile.</p>
         </li>
       </ul>
     </section>
