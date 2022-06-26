5454de50cedbd4568b5d98d52da323d77a0b22c3
rpashev <56789393+rpashev@users.noreply.github.com>
2022-06-26 22:24:56 +0300

    Delete src/Components directory

--- src/Components/UI/Backdrop.js
+++
@@ -1,7 +0,0 @@
-import classes from "./Backdrop.module.scss";
-
-const Backdrop = (props) => {
-  return <div className={classes.backdrop} onClick={props.onClose}></div>;
-};
-
-export default Backdrop;
--- src/Components/UI/Badge.js
+++
@@ -1,9 +0,0 @@
-import styles from "./Badge.module.scss";
-
-const Badge = (props) => {
-  return (
-    <span className={`${styles.badge} ${props.mobile ? styles.mobile : ""}`}>{props.count}</span>
-  );
-};
-
-export default Badge;
